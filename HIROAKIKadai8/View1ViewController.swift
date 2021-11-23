991abda02b06678291c0d1f0dc7e42987969768c
hirokaissh <hirokai.saito1014@gmail.com>
2021-11-23 14:09:19 +0900

    Uisliderのアウトレット接続

--- HIROAKIKadai8/View1ViewController.swift
+++ HIROAKIKadai8/View1ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 class View1ViewController: UIViewController {
     @IBOutlet weak var view1Label: UILabel!
     @IBOutlet weak var view2Label: UILabel!
+    @IBOutlet weak var view1Slider: UISlider!
+    @IBOutlet weak var view2Slider: UISlider!
 
     override func viewDidLoad() {
         super.viewDidLoad()
