5e00448bef26fbedddd89cf6073c7a65e39f8d2f
hirokaissh <hirokai.saito1014@gmail.com>
2021-11-23 21:11:06 +0900

    表示に関する機能の実装

--- HIROAKIKadai8/View1ViewController.swift
+++ HIROAKIKadai8/View1ViewController.swift
@@ -11,16 +11,23 @@ class View1ViewController: UIViewController {
     @IBOutlet private weak var view1Label: UILabel!
     @IBOutlet private weak var view1Slider: UISlider!
 
-    let valueKEY = "value"
+    private let valueKEY = "value"
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view1Label.text = UserDefaults.standard.string(forKey: valueKEY) ?? "0"
+        view1Label.text = "0.0"
+        view1Slider.value = 0.0
     }
 
     @IBAction private func view1SliderAction(_ sender: Any) {
-        let view1Value = String(view1Slider.value)
-        view1Label.text = view1Value
+        let view1Value = view1Slider.value
+        view1Label.text = String(view1Value)
         UserDefaults.standard.set(view1Value, forKey: valueKEY)
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        let value = UserDefaults.standard.float(forKey: valueKEY)
+        view1Label.text = String(value)
+        view1Slider.value = value
     }
 }
--- HIROAKIKadai8/View2ViewController.swift
+++ HIROAKIKadai8/View2ViewController.swift
@@ -11,8 +11,23 @@ class View2ViewController: UIViewController {
     @IBOutlet private weak var view2Label: UILabel!
     @IBOutlet private weak var view2Slider: UISlider!
 
+    private let valueKEY = "value"
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        view2Label.text = String(view2Slider.value)
+        view2Label.text = "0.0"
+        view2Slider.value = 0.0
+    }
+
+    @IBAction private func view2SliderAction(_ sender: Any) {
+        let view2Value = view2Slider.value
+        view2Label.text = String(view2Value)
+        UserDefaults.standard.set(view2Value, forKey: valueKEY)
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        let value = UserDefaults.standard.float(forKey: valueKEY)
+        view2Label.text = String(value)
+        view2Slider.value = value
     }
 }
