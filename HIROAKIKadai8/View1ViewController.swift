99a2d6c679f2eb159ebbf075f3b155fe72d44970
hirokaissh <hirokai.saito1014@gmail.com>
2021-11-23 14:30:07 +0900

    Valueが変わった時のラベルを更新する機能の追加

--- HIROAKIKadai8/View1ViewController.swift
+++ HIROAKIKadai8/View1ViewController.swift
@@ -11,9 +11,16 @@ class View1ViewController: UIViewController {
     @IBOutlet private weak var view1Label: UILabel!
     @IBOutlet private weak var view1Slider: UISlider!
 
+    let valueKEY = "value"
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view1Label.text = String(view1Slider.value)
+        view1Label.text = UserDefaults.standard.string(forKey: valueKEY) ?? "0"
+    }
+
+    @IBAction private func view1SliderAction(_ sender: Any) {
+        let view1Value = String(view1Slider.value)
+        view1Label.text = view1Value
+        UserDefaults.standard.set(view1Value, forKey: valueKEY)
     }
 }
