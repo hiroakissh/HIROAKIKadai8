91d79905299cd949fc93031ae5e9898ea71817cb
hirokaissh <hirokai.saito1014@gmail.com>
2021-11-23 14:19:06 +0900

    Viewの分離

--- HIROAKIKadai8/View1ViewController.swift
+++ HIROAKIKadai8/View1ViewController.swift
@@ -9,12 +9,11 @@ import UIKit
 
 class View1ViewController: UIViewController {
     @IBOutlet private weak var view1Label: UILabel!
-    @IBOutlet private weak var view2Label: UILabel!
     @IBOutlet private weak var view1Slider: UISlider!
-    @IBOutlet private weak var view2Slider: UISlider!
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        view1Label.text = String(view1Slider.value)
     }
 }
--- HIROAKIKadai8/View2ViewController.swift
+++ HIROAKIKadai8/View2ViewController.swift
@@ -8,22 +8,11 @@
 import UIKit
 
 class View2ViewController: UIViewController {
+    @IBOutlet private weak var view2Label: UILabel!
+    @IBOutlet private weak var view2Slider: UISlider!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view2Label.text = String(view2Slider.value)
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
